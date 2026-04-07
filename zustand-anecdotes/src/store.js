d7a28d5815dc59b165a4151ddad99df85cec381c
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-08 10:49:12 +1200

    [feat]ui: Implement initialize action

--- zustand-anecdotes/src/store.js
+++ zustand-anecdotes/src/store.js
@@ -1,13 +1,5 @@
-import { create } from 'zustand'
-
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+import { create } from 'zustand';
+import service from './services/anecdotes'
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -18,7 +10,7 @@ const asObject = anecdote => ({
 })
 
 const useAnecdoteStore = create((set) => ({
-  anecdotes: anecdotesAtStart.map(asObject),
+  anecdotes: [],
   filter: null,
   actions: {
     vote: (id) => set(state => ({
@@ -30,7 +22,11 @@ const useAnecdoteStore = create((set) => ({
     add: (anecdote) => set(state => ({
       anecdotes: [...state.anecdotes, asObject(anecdote)]
     })),
-    setFilter: (newFilter) => set(()=> ({filter: newFilter.trim().toLowerCase()}))
+    setFilter: (newFilter) => set(()=> ({filter: newFilter.trim().toLowerCase()})),
+    initialize: async () => {
+      const anecdotes = await service.getAll()
+      set(() => ({anecdotes}))
+    }
   },
 }))
 
