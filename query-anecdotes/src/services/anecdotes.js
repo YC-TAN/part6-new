b33b0e7ade6019c91699994076a5ac9fe5ab46a1
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-10 13:47:41 +1200

    [feat]ui: Implement anecdote services

--- query-anecdotes/src/services/anecdotes.js
+++ query-anecdotes/src/services/anecdotes.js
@@ -1,4 +1,4 @@
-const base_url = '/api/anecdotes'
+const base_url = 'http://localhost:3001/anecdotes'
 
 const getAll = async () => {
     const res = await fetch(base_url);
@@ -10,7 +10,40 @@ const getAll = async () => {
     return await res.json()
 }
 
+const create = async (newAnecdote) => {
+    const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(newAnecdote)
+    }
+
+    const res = await fetch(base_url, options)
+
+    if (!res.ok) {
+        throw new Error('Failed to create resource.')
+    }
+
+    return await res.json()
+
+}
+
+const update = async (updatedAnecdote) => {
+    const options = {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(updatedAnecdote)
+    }
+
+    const res = await fetch(`${base_url}/${updatedAnecdote.id}`, options)
+    if (!res.ok) {
+        throw new Error('Failed to create resource.')
+    }
+
+    return await res.json()
+}
 
 export {
     getAll,
+    create,
+    update
 }
